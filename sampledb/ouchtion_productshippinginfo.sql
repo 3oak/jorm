aecb02f24629c94d0228858bfb40f888c45078a1
Hung Cao <caogiahung11A2@gmail.com>
2022-01-04 02:53:54 +0700

    (!): Update Schema

--- sampledb/ouchtion_productshippinginfo.sql
+++ sampledb/ouchtion_productshippinginfo.sql
@@ -16,30 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `category`
+-- Table structure for table `productshippinginfo`
 --
 
-DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `productshippinginfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `parent_category_id` int DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`category_id`),
-  KEY `SUBCATEGORY_CATEGORY_idx` (`parent_category_id`),
-  CONSTRAINT `SUBCATEGORY_CATEGORY` FOREIGN KEY (`parent_category_id`) REFERENCES `category` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `productshippinginfo` (
+  `id` int NOT NULL,
+  `receiverName` varchar(50) DEFAULT NULL,
+  `receiverAddress` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `productshippinginfo`
 --
 
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,NULL,'Điện tử'),(2,1,'Điện thoại di động'),(3,1,'Laptop'),(4,1,'Tivi'),(5,NULL,'Sức khỏe'),(6,5,'Thực phẩm chức năng'),(7,5,'Y tế'),(8,5,'Thuốc'),(9,NULL,'Đồng hồ'),(10,9,'Đồng hồ nam'),(11,9,'Đồng hồ nữ'),(12,NULL,'Xe'),(13,12,'Xe đạp'),(14,NULL,'Máy ảnh & phụ kiện máy ảnh'),(15,14,'Máy ảnh'),(16,14,'Ống kính');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+LOCK TABLES `productshippinginfo` WRITE;
+/*!40000 ALTER TABLE `productshippinginfo` DISABLE KEYS */;
+INSERT INTO `productshippinginfo` VALUES (1,'Calvin','40 Messerschmidt Lane'),(2,'Kriste','897 Gulseth Point'),(3,'Anabel','0 3rd Drive'),(4,'Brien','317 Kropf Street'),(5,'Darlene','60 Shasta Crossing'),(6,'Celestia','4230 Helena Circle'),(7,'Quinton','15805 3rd Center'),(8,'Hildegaard','958 Moulton Lane'),(9,'Nappy','126 Roxbury Parkway'),(10,'Pepe','0 Elka Crossing');
+/*!40000 ALTER TABLE `productshippinginfo` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-03 22:06:35
+-- Dump completed on 2022-01-04  2:51:35
