b57526fcd9988e767c4046f58c544e59e3bafcc3
Phung Bui <alex.bui.vhp@gmail.com>
2022-01-06 00:53:05 +0700

    Fix executor remove strategy

--- src/jorm/connection/Connectable.java
+++ src/jorm/connection/Connectable.java
@@ -3,25 +3,24 @@ package jorm.connection;
 import java.sql.*;
 
 import jorm.connection.configuration.Configuration;
+import jorm.connection.configuration.MySQLConfiguration;
 import jorm.query.Queryable;
 
 @SuppressWarnings("unused")
 public abstract class Connectable {
     protected Connection connection;
 
-    protected String connectionURL;
-    protected String username;
-    protected String password;
+    public void OpenConnection(String connectionURL)
+            throws SQLException {
+        connection = DriverManager.getConnection(connectionURL);
+    }
 
-    private void OpenConnectionWithHost()
+    public void OpenConnection(String connectionURL, String username, String password)
             throws SQLException {
         connection = DriverManager.getConnection(connectionURL, username, password);
     }
 
-    private void OpenConnectionWithoutHost()
-            throws SQLException {
-        connection = DriverManager.getConnection(connectionURL);
-    }
+    public abstract void OpenConnection(Configuration configuration) throws SQLException;
 
     public abstract <T> Queryable<T> CreateQuery(Class<T> userClass)
             throws RuntimeException;
@@ -29,33 +28,6 @@ public abstract class Connectable {
     public Connection GetConnection() {
         return connection;
     }
-
-    /* *** TEMPLATE METHOD *** */
-    public void OpenConnection(String connectionURL)
-            throws SQLException {
-        this.connectionURL = connectionURL;
-
-        OpenConnectionWithoutHost();
-    }
-
-    public void OpenConnection(String connectionURL, String username, String password)
-            throws SQLException {
-        this.connectionURL = connectionURL;
-        this.username = username;
-        this.password = password;
-
-        OpenConnectionWithHost();
-    }
-
-    public void OpenConnection(Configuration configuration)
-            throws SQLException {
-        this.connectionURL = configuration.GetConnectionURL();
-        this.username = configuration.username;
-        this.password = configuration.password;
-
-        OpenConnectionWithHost();
-    }
-    /* *********************** */
 
     public void CloseConnection()
             throws SQLException {
