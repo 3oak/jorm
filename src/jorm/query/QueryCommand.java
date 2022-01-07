f030cd2eb5420a62de8fb1736457a976f733770a
Hung Cao <caogiahung11A2@gmail.com>
2022-01-07 16:14:58 +0700

    Change Queue to PriorityQueue

--- src/jorm/query/QueryCommand.java
+++ src/jorm/query/QueryCommand.java
@@ -2,16 +2,14 @@ package jorm.query;
 
 import jorm.utils.Tuple;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class QueryCommand {
-    public final Queue<Tuple<QueryType, String>> commandQueue;
+    public final PriorityQueue<Tuple<QueryType, String>> commandQueue;
 
     public QueryCommand() {
-        commandQueue = new LinkedList<>();
+        commandQueue = new PriorityQueue<>(new TupleComparator());
     }
 
     public void AddCommand(Tuple<QueryType, String> command) {
@@ -31,4 +29,66 @@ public class QueryCommand {
     public void Clear() {
         commandQueue.clear();
     }
+}
+
+class TupleComparator implements Comparator<Tuple<QueryType, String>> {
+
+    @Override
+    public int compare(Tuple<QueryType, String> o1, Tuple<QueryType, String> o2) {
+        QueryLevel queryLevel1 = null, queryLevel2 = null;
+
+        switch (o1.item1) {
+            case SELECT:
+            case UPDATE:
+            case INSERT:
+            case DELETE:
+                queryLevel1 = QueryLevel.COMMANDS;
+                break;
+            case WHERE:
+                queryLevel1 = QueryLevel.WHERE;
+                break;
+            case AND:
+            case OR:
+                queryLevel1 = QueryLevel.CONDITION;
+                break;
+            case FIELDS:
+                queryLevel1 = QueryLevel.FIELD;
+                break;
+        }
+
+        switch (o2.item1) {
+            case SELECT:
+            case UPDATE:
+            case INSERT:
+            case DELETE:
+                queryLevel2 = QueryLevel.COMMANDS;
+                break;
+            case WHERE:
+                queryLevel2 = QueryLevel.WHERE;
+                break;
+            case AND:
+            case OR:
+                queryLevel2 = QueryLevel.CONDITION;
+                break;
+            case FIELDS:
+                queryLevel2 = QueryLevel.FIELD;
+                break;
+        }
+
+        if (queryLevel1.ordinal() < queryLevel2.ordinal()) {
+            return 1;
+        } else if (queryLevel1.ordinal() > queryLevel2.ordinal()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+}
+
+enum QueryLevel {
+    CONDITION,
+    WHERE,
+    FIELD,
+    COMMANDS,
 }
